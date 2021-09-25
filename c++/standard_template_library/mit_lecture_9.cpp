b7e2e7116b7de5af74106d8f802cbc0df7aaf11c
violetcodes <meetvsl812@gmail.com>
2021-09-25 20:15:41 +0530

    some_updates

--- c++/standard_template_library/mit_lecture_9.cpp
+++ c++/standard_template_library/mit_lecture_9.cpp
@@ -1,11 +1,74 @@
 #include <iostream> 
+#include <string>
+#include <cmath>
 using namespace std; 
 
-int sum(const int x, const int y) {
-    return x+y;
+
+
+template <typename T>
+T sum(const T a, const T b) {
+    return a+b;
 }
 
+
+template <typename T>
+class Point {
+    private:
+        T x, y;
+    
+    public:
+        Point(const T u, const T v): x(u), y(v) {};
+        T get_x() {return x;}
+        T get_y() {return y;}
+        void get_xandy();
+};
+
+template <typename T>
+void Point<T>::get_xandy() {
+    cout << "(" << x << ", " << y << ")" << endl;
+}
+
+template <typename T>
+class Container {
+    private:
+        T element; 
+    
+    public:
+        Container(T elem): element(elem) {};
+        void print_it() {
+            cout << element << endl;
+        }
+};
+
+template <>
+class Container<string> {
+    private:
+        string elem;
+    
+    public:
+        Container(string e): elem(e) {};
+        void print_it() {
+            cout << elem << endl;
+        }
+
+};
+
 int main() {
-    cout<<sum(3, 5)<<endl;
-    return 0;
+
+    Container<string> c1 = Container<string>("hello, Vishal");
+    c1.print_it();
+
+    Container<int> c2 = Container<int>(32);
+    c2.print_it();
+    
+    // Point<int> p1 = Point<int>(3, 2);
+    // cout << "X:" << p1.get_x() << " " << "Y:" << p1.get_y() << endl;
+    // p1.get_xandy();
+
+
+    // Point<int> p2 = Point<int>(1, 1);
+    // cout << "Distance from p2(1,1):" << p1.distance(p2) << endl;
+    
+    // cout<<sum(3.1, 5.9)<<endl;
+    // return 0;
 }
