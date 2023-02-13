b00bc1c2984a130c2462877e00de883bd9b4a1fb
violetcodes <meetvsl812@gmail.com>
2023-02-13 13:20:53 +0530

    Finish bbst

--- mit_6006/bbst.py
+++ mit_6006/bbst.py
@@ -1,5 +1,3 @@
-
-
 class BSTNode:
     @staticmethod
     def height_fn(node):
@@ -31,82 +29,71 @@ class BSTNode:
         if self.right is None: return self 
         return self.right._right_most_child()
 
-    def _first_left_ancestor(self, ):
-        parent = self.parent 
-        if parent is None:
-            return None 
-        if parent.left == self:
-            return parent
-        return parent._first_left_ancestor()
-
     def _first_right_ancestor(self, ):
-        parent = self.parent 
-        if parent is None:
-            return None 
-        if parent.right == self:
-            return parent
-        return parent._first_right_ancestor()
+        while (self.parent is not None) and (self.parent.right is self):
+            self = self.parent 
+        return self.parent 
+        
+
+    def _first_left_ancestor(self, ):
+        while (self.parent is not None) and (self.parent.left is self):
+            self = self.parent 
+        return self.parent 
 
 
     def find_next(self, ):
         if self.right is not None:
             return self.right._left_most_child()
-        return self._first_left_ancestor()
+        return self._first_right_ancestor()
 
     def find_prev(self, ):
         if self.left is not None:
             return self.left._right_most_child()
-        return self._first_right_ancestor() 
+        return self._first_left_ancestor() 
 
     def insert_next(self, node):
         if self.right is None:
-            self.right = node 
-            node.parent = self 
-            return 
+            self.right, node.parent = node, self 
+        else:
+            left_most_child = self.right._left_most_child()
+            left_most_child.left, node.parent = node, left_most_child
+        
+        node._balance() 
 
-        left_most_child = self.right._left_most_child()
-        left_most_child.left = node 
-        node.parent = left_most_child 
-
-        # left_most_child._balance() 
-        # self._maintain() 
+        
 
     def insert_prev(self, node):  # O(logH)
         if self.left is None:
-            self.left = node 
-            node.parent = self 
-            return 
-        
-        right_most_child = self.left._right_most_child()
-        right_most_child.right = node 
-        node.parent = right_most_child
+            self.left, node.parent = node, self
+        else:
+            right_most_child = self.left._right_most_child()
+            right_most_child.right, node.parent = node, right_most_child
 
-        # left_most_child._balance() 
-        # self._maintain()
+        node._balance()
 
     def insert_node(self, node):
         if node.item < self.item:
             if self.left is None:
-                self.left = node 
-                node.parent = self 
+                self.left, node.parent = node, self
             else:
                 self.left.insert_node(node)
+
         elif node.item > self.item:
             if self.right is None:
-                self.right = node 
-                node.parent = self 
+                self.right, node.parent = node, self
             else:
                 self.right.insert_node(node)
             
         else:
             if self.left is None:
-                self.left = node 
-                node.parent = self 
+                self.left, node.parent = node, self
             elif self.right is None:
-                self.right = node 
-                node.parent = self 
+                self.right, node.parent = node, self 
             else:
                 self.left.insert_node(node)
+                
+        node._balance()
+        
 
 
     @property
@@ -118,22 +105,21 @@ class BSTNode:
 
     def remove_node(self, ):  # O(logH)
         if self._is_leaf:
-            p = self.parent
-            if p.left is self: p.left = None 
-            else: p.right = None 
-            del self 
-            return 
+            if self.parent.left is self: self.parent.left = None 
+            else: self.parent.right = None 
+            self.parent._balance()
+            return self
         
         if self.left is not None:
             prev_node = self.find_prev()
             prev_node._swap(self)
-            prev_node.remove_node()
-            return 
+            return prev_node.remove_node()
         
         else:
             next_node = self.find_next()
             next_node._swap(self)
-            next_node.remove_node() 
+            return next_node.remove_node() 
+
     
     def rotate_left(self, ): # O(1)
         right = self.right 
@@ -153,6 +139,7 @@ class BSTNode:
             if parent.left == self: parent.left = right 
             else: parent.right = right 
         right.parent = parent 
+        return right 
 
     def rotate_right(self, ):  # O(1)
         left = self.left
@@ -171,27 +158,29 @@ class BSTNode:
         if parent is not None:
             if parent.left is self: parent.left = left 
             else: parent.right = left 
+        left.parent = parent 
+        return left 
 
 
     def _is_balanced(self, ):
         return (
-            (self._skew in (-1, 0, 1)) 
+            (self.skew in (-1, 0, 1)) 
             and ((self.left is None) or self.left._is_balanced()) 
             and ((self.right is None) or self.right._is_balanced())
         )
 
     
     
-    def _show(self, ): # Not tested
-        backslash_char = '\\'
-        a = '' if self.parent is None else str({self.parent.item})
+    def __str__(self, ): 
+        backslash_char, pipe = '\\', '|'
+        a = '' if self.parent is None else str(self.parent.item)
         a = f"{a:^20}" + '\n'
-        a + f"{'|':^20}" + '\n'
+        a += f"{pipe:^20}" + '\n'
         a += f"{str(self.item):^20}" + '\n'
         a += f"{'/':^10}" + f"{backslash_char:^10}" + '\n'
         l, r = map(lambda k: ('' if k is None else str(k.item)), [self.left, self.right])
-        a += f"{l:^10}" + f"{r:^10}"
-        return a 
+        a += f"{l:^8}" + '    ' + f"{r:^8}" + '\n'
+        return a
 
     
     @staticmethod
@@ -221,13 +210,41 @@ class BSTNode:
         if node is None:
             return []
         return BSTNode.inorder_t(node.left) + [node, ] + BSTNode.inorder_t(node.right)
+
+    def _local_balance(self, ):  # O(1)
+        if self.skew >= 2:
+            if (self.right is not None) or (self.right.skew < 0):
+                self.right.rotate_right()
+            self.rotate_left()
+        elif self.skew <= -2:
+            if (self.left is not None) or (self.left.skew > 0):
+                self.left.rotate_left()
+            self.rotate_right()
+            
+                
+                
+    
+    def _balance(self, ):
+        while self is not None:
+            self._local_balance()
+            self = self.parent 
+
         
         
 
 
 class BST:
     def __init__(self, root=None):
-        self.root = root 
+        self._root = root 
+
+    @property
+    def root(self, ):
+        while self._root.parent is not None:
+            self._root = self._root.parent 
+        return self._root 
+        
+        
+            
 
     @classmethod
     def _from_arr(cls, arr):
@@ -238,23 +255,14 @@ class BST:
         return self.root._left_most_child()
 
     def _show(self, ):
-        nodes = BSTNode.inorder_t(self.root)
+        nodes = [(i, node) for i, node in enumerate(BSTNode.inorder_t(self.root))]
         fn = lambda x: (x.item if x is not None else " ")
         print("="*60)
-        for i, node in enumerate(nodes):
+        for i, node in sorted(nodes, key=lambda x: -x[-1].height):
             print(
-                f"{i}: {node.item}, "
-                f"parent: {fn(node.parent)}, height: {node.height}, skew: {node.skew}"
+                f"{i}: parent: {fn(node.parent)}, node: {node.item}, "
+                f"height: {node.height}, skew: {node.skew}"
             )
-
-    
-        
-
-
-
-
-
-
 
 
 def test():
@@ -266,8 +274,8 @@ def test():
         array = sorted(array)
         bst = BST._from_arr(array)
 
-        print('In order traversal')
-        bst._show()
+        # print('In order traversal')
+        # bst._show()
         # this checks for build, height, skew, find next and prev and other functions 
 
         # next let's check for insert and delete 
@@ -309,14 +317,37 @@ def test():
 
         # delete 
         # remove 9 and check right of 5 and it's children 
-        # nodes[8].remove_node() 
-        # print('new right to 5:', nodes[5].right.item)
-        # print('new next of 8:', nodes[7].find_next().item)
+        nodes[8].remove_node() 
+        print('new right to 5:', nodes[5].right.item)
+        nodes = {i: node for i, node in enumerate(BSTNode.inorder_t(bst.root))}
+        print('new next of 8:', nodes[7].find_next().item)
+        bst._show()
+
+        # bst.root.remove_node()
+        # bst._show()
+        
+        # # rotations 
+        # # root left rotate 
+        # bst.root = bst.root.rotate_left()
         # bst._show()
 
-        bst.root.remove_node()
-        bst._show()
-        
+        # # root right rotate
+        # bst.root = bst.root.rotate_right()
+        # bst._show()
+
+        # nodes[8].rotate_right()
+        # bst._show()
+
+        # print(nodes[8])
+
+
+        # print('balanced add')
+        # bst = BST(BSTNode(array[0]))
+        # for a in array[1:]:
+        #     bst.root.insert_node(BSTNode(a))
+
+        # bst._show()
+        # print(bst._root._is_balanced())
 
 
 
@@ -324,4 +355,6 @@ def test():
         import pdb, traceback
         print(traceback.print_exc())
         pdb.set_trace()
-test() 
+
+if __name__ == '__main__':
+    test() 
